4a76e2e81ac6a53c889eaf4908f911d5b0b8f2db
Bubba Godinet <bubba.godinet@M235.local>
2024-07-15 22:39:52 -0600

    updated snippets

--- src/app/sphere-attraction/page.tsx
+++ src/app/sphere-attraction/page.tsx
@@ -4,12 +4,8 @@ import tw from 'twin.macro';
 
 export default function SphereAttraction() {
 return (
-<HerroButton>
+<>
   Herro
-</HerroButton>
+</>
 )
 }
-
-const HerroButton = tw.div`
-  bg-purple
-`
